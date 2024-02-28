e44260b3505b1666a87ff519235dad15bc1dd007
Arif Tazegüllü <atazegullu@gmail.com>
2024-02-28 13:14:54 +0300

    fix calculation

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 function  Calculate(){
     let kg = document.getElementById("kg").value;
-    let ml = document.getElementById("ml").value;
+    let mg = document.getElementById("mg").value;
+    let mgin5ml = document.getElementById("mgin5ml").value;
 
-    if(kg != "" && ml!=""){
-        document.getElementById("total").innerText = "Toplam: " + kg*ml;
-        document.getElementById("doz").innerText = "Doz: " + kg*ml/5;
+    let total = kg*mg;
+    let totalOlcek = total * 5 / mgin5ml;
+    let kacOlcek = total / mgin5ml;
+
+    if(kg != "" && mg!="" && mgin5ml!=""){
+        document.getElementById("total").innerText = "Toplam MG: " + total;
+        document.getElementById("totalOlcek").innerText = "Toplam İlaç ML: " + totalOlcek;
+        document.getElementById("kacOlcek").innerText = "Ölçek Sayısı: " + kacOlcek;
     }
     else{
         document.getElementById("total").innerText = "";
-        document.getElementById("doz").innerText = "";
+        document.getElementById("totalOlcek").innerText = "";
+        document.getElementById("kacOlcek").innerText = "";
     }
 }
